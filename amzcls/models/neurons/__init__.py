cbbffa704ca6c2d597ae8f8927992cbb25ce484c
StephenTaylor1998 <2684109034@qq.com>
2023-01-24 21:11:14 +0800

    neuron

---
+++ amzcls/models/neurons/__init__.py
@@ -0,0 +1,19 @@
+from .builder import (
+    NODES, SURROGATE, build_node, build_surrogate
+)
+from .node import (
+    IFNode, LIFNode, ParametricLIFNode, QIFNode, EIFNode, IzhikevichNode, LIAFNode
+)
+from .surrogate import (
+    PiecewiseQuadratic, PiecewiseExp, Sigmoid, SoftSign, ATan, NonzeroSignLogAbs,
+    Erf, PiecewiseLeakyReLU, SquarewaveFourierSeries, S2NN, QPseudoSpike, LeakyKReLU,
+    FakeNumericalGradient, LogTailedReLU
+)
+
+__all__ = [
+    'NODES', 'SURROGATE', 'build_node', 'build_surrogate',
+    'IFNode', 'LIFNode', 'ParametricLIFNode', 'QIFNode', 'EIFNode', 'IzhikevichNode', 'LIAFNode',
+    'PiecewiseQuadratic', 'PiecewiseExp', 'Sigmoid', 'SoftSign', 'ATan', 'NonzeroSignLogAbs',
+    'Erf', 'PiecewiseLeakyReLU', 'SquarewaveFourierSeries', 'S2NN', 'QPseudoSpike', 'LeakyKReLU',
+    'FakeNumericalGradient', 'LogTailedReLU'
+]
