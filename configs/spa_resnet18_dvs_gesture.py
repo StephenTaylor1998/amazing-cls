0cbfae41b99a680e49f25304f2df483ae6140bf6
StephenTaylor1998 <2684109034@qq.com>
2023-01-27 14:35:51 +0800

    update

---
+++ configs/spa_resnet18_dvs_gesture.py
@@ -0,0 +1,34 @@
+_base_ = [
+    './_base_/models/spa_resnet18_cifar.py', './_base_/datasets/dvs_gesture_t4.py',
+    './_base_/schedules/cifar10_bs128.py', './_base_/default_runtime.py'
+]
+
+# 88.2500%
+model = dict(
+    type='ImageClassifier',
+    backbone=dict(
+        type='SpikePreActResNetCifar',
+        block_type='SpikePreActBasicBlock',
+        layers=[2, 2, 2, 2],
+        width=[64, 128, 256, 512],
+        num_classes=11,
+        in_channels=2,
+        zero_init_residual=True,
+        groups=1, width_per_group=64,
+        replace_stride_with_dilation=None,
+        norm_layer=None,
+        neuron_cfg=dict(
+            type='IFNode',
+            surrogate_function=dict(
+                type='ATan'
+            )
+        ),
+    ),
+    head=dict(
+        type='ClsHead',
+        cal_acc=True,
+        loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+    )
+)
+
+
