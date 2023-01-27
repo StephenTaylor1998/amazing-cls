632a078bd8c8463c946d0e2ed0ceb3b9141257ee
StephenTaylor1998 <2684109034@qq.com>
2023-01-27 14:34:38 +0800

    update

--- amzcls/datasets/__init__.py
+++ amzcls/datasets/__init__.py
@@ -1,8 +1,10 @@
 from .builder import (DATASETS, PIPELINES, SAMPLERS, build_dataloader,
                       build_dataset, build_sampler)
+from .dvs_gesture import DVSGesture
+from .dvs_cifar10 import DVSCifar10
 
 from .piplines.encoding import ToTime
 __all__ = [
     'build_dataloader', 'build_dataset', 'DATASETS', 'PIPELINES', 'SAMPLERS',
-    'build_sampler', 'ToTime'
+    'build_sampler', 'DVSGesture', 'DVSCifar10', 'ToTime'
 ]
