f898ea14d27dc4bc73d2b477ccdb7a5aacab8411
StephenTaylor1998 <2684109034@qq.com>
2023-01-28 15:08:39 +0800

    update dvs cifar10 config

---
+++ configs/dvs_cifar10/org4dvs_cifar10.py
@@ -0,0 +1,26 @@
+_base_ = [
+    '../_base_/datasets/dvs_cifar10_t16.py',
+    '../_base_/schedules/cifar10_bs128.py',
+    '../_base_/default_runtime.py'
+]
+
+# 88.2500%
+model = dict(
+    type='ImageClassifier',
+    backbone=dict(
+        type='org4dvs_cifar10'
+    ),
+    head=dict(
+        type='ClsHead',
+        cal_acc=True,
+        loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+    )
+)
+
+optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=1e-5)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(policy='step', step=[64, 128])
+runner = dict(type='EpochBasedRunner', max_epochs=192)
+
+
