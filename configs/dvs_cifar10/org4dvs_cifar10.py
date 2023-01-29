e2ba70043ab2faad02057bae4b110b4f3dadbc21
StephenTaylor1998 <2684109034@qq.com>
2023-01-29 22:01:04 +0800

    update dvs cifar10

--- configs/dvs_cifar10/org4dvs_cifar10.py
+++ configs/dvs_cifar10/org4dvs_cifar10.py
@@ -20,7 +20,14 @@ model = dict(
 optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=1e-5)
 optimizer_config = dict(grad_clip=None)
 # learning policy
-lr_config = dict(policy='step', step=[64, 128])
-runner = dict(type='EpochBasedRunner', max_epochs=192)
+lr_config = dict(
+    policy='CosineRestart',
+    periods=[30, 60, 150],
+    restart_weights=[1., 0.9, 0.6],
+    min_lr_ratio=0.001,
+    warmup='linear',
+    warmup_iters=500,
+)
+runner = dict(type='EpochBasedRunner', max_epochs=150)
 
 
