801c1409026552bacce8306675907f46c9affdb0
StephenTaylor1998 <2684109034@qq.com>
2023-01-29 22:01:16 +0800

    update dvs gesture

--- configs/dvs_gesture/sew4dvs_gesture.py
+++ configs/dvs_gesture/sew4dvs_gesture.py
@@ -1,6 +1,5 @@
 _base_ = [
     '../_base_/datasets/dvs_gesture_t16.py',
-    '../_base_/schedules/cifar10_bs128.py',
     '../_base_/default_runtime.py'
 ]
 
@@ -9,7 +8,7 @@ model = dict(
     type='ImageClassifier',
     backbone=dict(
         type='sew4dvs_gesture',
-        cnf='add'
+        cnf='or'
     ),
     head=dict(
         type='ClsHead',
@@ -21,7 +20,14 @@ model = dict(
 optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=1e-7)
 optimizer_config = dict(grad_clip=None)
 # learning policy
-lr_config = dict(policy='step', step=[64, 128])
-runner = dict(type='EpochBasedRunner', max_epochs=192)
+lr_config = dict(
+    policy='CosineRestart',
+    periods=[30, 60, 150],
+    restart_weights=[1., 0.9, 0.6],
+    min_lr_ratio=0.001,
+    warmup='linear',
+    warmup_iters=500,
+)
+runner = dict(type='EpochBasedRunner', max_epochs=150)
 
 
