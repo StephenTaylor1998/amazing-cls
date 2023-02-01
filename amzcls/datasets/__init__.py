36be503a185f092bf5b3b8c918af2059d9e35a02
StephenTaylor1998 <2684109034@qq.com>
2023-02-01 23:42:05 +0800

    tsd

--- amzcls/datasets/__init__.py
+++ amzcls/datasets/__init__.py
@@ -2,9 +2,11 @@ from .builder import (DATASETS, PIPELINES, SAMPLERS, build_dataloader,
                       build_dataset, build_sampler)
 from .dvs_gesture import DVSGesture
 from .dvs_cifar10 import DVSCifar10
+from .tsd import TimeSeqDataset
 
 from .piplines.encoding import ToTime, ToFloatTensor, TimeSample
 __all__ = [
     'build_dataloader', 'build_dataset', 'DATASETS', 'PIPELINES', 'SAMPLERS',
-    'build_sampler', 'DVSGesture', 'DVSCifar10', 'ToTime', 'ToFloatTensor', 'TimeSample'
+    'build_sampler', 'DVSGesture', 'DVSCifar10', 'TimeSeqDataset',
+    'ToTime', 'ToFloatTensor', 'TimeSample'
 ]
