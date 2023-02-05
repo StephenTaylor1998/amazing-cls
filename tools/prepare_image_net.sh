79b9f05217cad701664ee13c56cedac517444e1a
StephenTaylor1998 <2684109034@qq.com>
2023-02-05 22:34:37 +0800

    fixed

--- tools/prepare_image_net.sh
+++ tools/prepare_image_net.sh
@@ -4,9 +4,13 @@ tar xvf ILSVRC2012_img_train.tar -C ./train
 tar xvf ILSVRC2012_img_val.tar -C ./val
 
 dir=./train
-for x in `ls $dir/*tar` do
+for x in `ls $dir/*tar`
+do
   filename=`basename $x .tar`
   mkdir $dir/$filename
   tar -xvf $x -C $dir/$filename
 done
+
+tar -xzf ILSVRC2012_devkit_t12.tar.gz
+
 #rm *.tar
