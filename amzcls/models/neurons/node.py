843d93d24fd0c16e819428f95010cb749407101f
StephenTaylor1998 <2684109034@qq.com>
2023-02-11 19:56:23 +0800

    TS1Node support

--- amzcls/models/neurons/node.py
+++ amzcls/models/neurons/node.py
@@ -1,6 +1,8 @@
+from mmcv.runner import auto_fp16
 from spikingjelly.activation_based.neuron import (
     IFNode, LIFNode, ParametricLIFNode, QIFNode, EIFNode, IzhikevichNode, LIAFNode
 )
+from torch import nn
 
 from .builder import NODES
 
@@ -14,3 +16,17 @@ from .builder import NODES
 
 for node in (IFNode, LIFNode, ParametricLIFNode, QIFNode, EIFNode, IzhikevichNode, LIAFNode):
     NODES.register_module(node)
+
+
+@NODES.register_module()
+class TS1Node(nn.Module):
+    def __init__(self, surrogate_function, threshold=1.):
+        super(TS1Node, self).__init__()
+        self.surrogate_function = surrogate_function
+        self.threshold = threshold
+        self.fp16_enabled = False
+
+    @auto_fp16(apply_to=('x',))
+    def forward(self, x):
+        # return torch.gt(x, self.threshold).to(x)
+        return self.surrogate_function(x - self.threshold)
