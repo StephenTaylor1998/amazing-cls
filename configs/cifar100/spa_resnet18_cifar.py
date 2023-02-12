b85a796c8421fc77f6d24d23809af1a700e83305
StephenTaylor1998 <2684109034@qq.com>
2023-02-12 23:03:48 +0800

    cifar100

---
+++ configs/cifar100/spa_resnet18_cifar.py
@@ -0,0 +1,10 @@
+_base_ = [
+    '../_base_/models/spa_resnet18_cifar.py', '../_base_/datasets/cifar10_bs512.py',
+    '../_base_/schedules/cifar10_bs128.py', '../_base_/default_runtime.py'
+]
+
+model = dict(
+    backbone=dict(
+        num_classes=100,
+    )
+)
