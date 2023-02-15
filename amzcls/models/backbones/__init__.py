43aad8efb273cbbcf1f14da994a06ba9f6819e9e
StephenTaylor1998 <2684109034@qq.com>
2023-02-15 21:41:40 +0800

    update

--- amzcls/models/backbones/__init__.py
+++ amzcls/models/backbones/__init__.py
@@ -3,7 +3,10 @@ from .base import (
     DualFlowBasicBlockA, DualFlowBottleneckA, DualFlowBasicBlockB, DualFlowBottleneckB,
     DualFlowBasicBlockC, DualFlowBottleneckC
 )
-from .plain_net import PlainNet
+from .plain_net import (
+    PlainDigital, plain_digital_a, plain_digital_b, plain_digital_c,
+    PlainAnalog, plain_analog_a, plain_analog_b, plain_analog_c
+)
 
 from .sew4dvs import (
     ResNetN4DVS,
@@ -11,13 +14,15 @@ from .sew4dvs import (
     sew4dvs_cifar10, org4dvs_cifar10, spk4dvs_cifar10, spa4dvs_cifar10,
     sew4tsd, org4tsd, spk4tsd, spa4tsd
 )
-from .spike_df_resnet import DFResNetCifar, df_resnet18c_cifar, df_resnet34c_cifar, df_resnet50c_cifar, \
-    df_resnet101c_cifar, df_resnet152c_cifar, df_resnext50c_cifar_32x4d, df_resnext101c_cifar_32x8d, \
-    df_wide_resnet50c_cifar_2, df_wide_resnet101c_cifar_2, df_resnet18b_cifar, df_resnet34b_cifar, df_resnet50b_cifar, \
-    df_resnet101b_cifar, df_resnet152b_cifar, df_resnext50b_cifar_32x4d, df_resnext101b_cifar_32x8d, \
-    df_wide_resnet50b_cifar_2, df_wide_resnet101b_cifar_2, df_resnet18a_cifar, df_resnet34a_cifar, df_resnet50a_cifar, \
-    df_resnet101a_cifar, df_resnet152a_cifar, df_resnext50a_cifar_32x4d, df_resnext101a_cifar_32x8d, \
-    df_wide_resnet50a_cifar_2, df_wide_resnet101a_cifar_2
+from .spike_df_resnet import (
+    DFResNetCifar,
+    df_resnet18c_cifar, df_resnet34c_cifar, df_resnet50c_cifar, df_resnet101c_cifar, df_resnet152c_cifar,
+    df_resnext50c_cifar_32x4d, df_resnext101c_cifar_32x8d, df_wide_resnet50c_cifar_2, df_wide_resnet101c_cifar_2,
+    df_resnet18b_cifar, df_resnet34b_cifar, df_resnet50b_cifar, df_resnet101b_cifar, df_resnet152b_cifar,
+    df_resnext50b_cifar_32x4d, df_resnext101b_cifar_32x8d, df_wide_resnet50b_cifar_2, df_wide_resnet101b_cifar_2,
+    df_resnet18a_cifar, df_resnet34a_cifar, df_resnet50a_cifar, df_resnet101a_cifar, df_resnet152a_cifar,
+    df_resnext50a_cifar_32x4d, df_resnext101a_cifar_32x8d, df_wide_resnet50a_cifar_2, df_wide_resnet101a_cifar_2
+)
 from .spike_preact_resnet import (
     SpikePreActResNetCifar, spike_preact_resnet18_cifar, spike_preact_resnet34_cifar,
     spike_preact_resnet50_cifar, spike_preact_resnet101_cifar, spike_preact_resnet152_cifar,
@@ -28,13 +33,13 @@ from .spike_resnet import (
     spike_resnet101_cifar, spike_resnet152_cifar, spike_resnext50_cifar_32x4d,
     spike_resnext101_cifar_32x8d, spike_wide_resnet50_cifar_2, spike_wide_resnet101_cifar_2)
 
-
 __all__ = [
     'SEWBasicBlock', 'SEWBottleneck', 'SpikePreActBasicBlock', 'SpikePreActBottleneck',
     'DualFlowBasicBlockA', 'DualFlowBottleneckA', 'DualFlowBasicBlockB', 'DualFlowBottleneckB',
     'DualFlowBasicBlockC', 'DualFlowBottleneckC',
 
-    'PlainNet',
+    'PlainDigital', 'plain_digital_a', 'plain_digital_b', 'plain_digital_c',
+    'PlainAnalog', 'plain_analog_a', 'plain_analog_b', 'plain_analog_c',
 
     'ResNetN4DVS',
     'sew4dvs_gesture', 'org4dvs_gesture', 'spk4dvs_gesture', 'spa4dvs_gesture',
